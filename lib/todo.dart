dd8f49b76ff2594a9744628acc1fc2b053171617
Pregum <pregum.y@gmail.com>
2022-07-24 04:05:27 +0900

    update: hiveに対応

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,8 +1,28 @@
+import 'package:hive/hive.dart';
+
+part 'todo.g.dart';
+
+@HiveType(typeId: 1)
+
+/// todo
 class Todo {
+  /// id
+  @HiveField(1)
   String id;
+
+  /// タスクの名前
+  @HiveField(2)
   String taskName;
+
+  /// タスクの完了フラグ
+  @HiveField(3)
   bool isCompleted;
+
+  /// タスクの編集中フラグ
+  @HiveField(4)
   bool isEditEnabled;
+
+  /// ctor
   Todo(
       {required this.id,
       required this.taskName,
